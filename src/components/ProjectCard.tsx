fd33347d52afbc3960f3c105c08b7c5125ab0210
mrlangloi <rcheung1997@gmail.com>
2026-01-16 12:54:31 -0800

    included project img

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,20 +6,26 @@ interface ProjectProps {
     link: string;
 };
 
-const ProjectCard = (props : ProjectProps) => {
+const ProjectCard = (props: ProjectProps) => {
 
-    const {title, description, tech, link} = props;
+    const { title, description, tech, link } = props;
 
     return (
         <div className="card">
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <div className="tech-stack tech-stack-wrapper">
-                {tech.map((t, index) => (
-                    <span key={index} className="tag">{t}</span>
-                ))}
+            <div className="card-image">
+                <img src="https://placehold.co/600x400/png"></img>
             </div>
-            <a href={link} target="_blank" rel="noopener noreferrer">Github</a>
+            <div className="card-content">
+                <h3>{title}</h3>
+                <p>{description}</p>
+                <div className="tech-stack tech-stack-wrapper">
+                    {tech.map((t, index) => (
+                        <span key={index} className="tag">{t}</span>
+                    ))}
+                </div>
+                <a href={link} target="_blank" rel="noopener noreferrer">Github</a>
+            </div>
+
         </div>
     );
 };
