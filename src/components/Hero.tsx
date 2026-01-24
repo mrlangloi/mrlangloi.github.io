817c991574c68bd1f75285785e0d6f8ca1a3c563
mrlangloi <rcheung1997@gmail.com>
2026-01-23 20:41:13 -0800

    added hero css modules

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,9 @@
+import styles from './Hero.module.css';
+
 const Hero = () => {
     return (
-        <section className="hero-section">
-            <div className="hero-content">
+        <section className={styles["hero-section"]}>
+            <div className={styles["hero-content"]}>
                 <h1>Hi, I'm Ricky!</h1>
                 <h2>I am a 3rd-year Computing Science SoSy Student @ SFU.</h2>
                 <h2>I have a knack for building applications and troubleshooting technical issues.</h2>
