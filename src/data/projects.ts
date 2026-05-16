4daff606a1ecb1aab410d3b81acb7bb9db64dfbc
Rcheu <rcheung1997@gmail.com>
2026-05-15 21:09:59 -0700

    updated projects list

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -8,17 +8,17 @@ export interface Project {
 
 const featuredProjectsList = [
     {
+        title: "OBS Canvas Overlay v2 (Work in Progress)",
+        description: "An updated OBS Canvas Overlay using TypeScript, React Compiler, Spring Boot, and PostgreSQL",
+        tech: ["TypeScript", "React", "Vite", "Node.js", "Java", "Spring Boot", "Socket.IO", "PostgreSQL", "HTML/CSS"],
+        link: "https://github.com/mrlangloi/obs-canvas"
+    },
+    {
         title: "Personal Calendar Tracker",
         description: "A RESTful personal calendar and tracker application developed using Java and Spring Boot",
         tech: ["Java", "Spring Boot", "Swing"],
         link: "https://github.com/mrlangloi/ImportantDaysTracker"
 
-    },
-    {
-        title: "OBS Canvas Overlay",
-        description: "An Open Broadcaster Software (OBS) overlay for live-stream moderators to display media to viewers",
-        tech: ["JavaScript", "React", "Node.js", "Express", "MongoDB"],
-        link: "https://github.com/mrlangloi/canvas-overlay-client"
     },
     {
         title: "Stock Image Searcher",
@@ -37,13 +37,13 @@ const allProjectsList = [
     {
         title: "OBS Canvas Overlay v2 (Work in Progress)",
         description: "An updated OBS Canvas Overlay using TypeScript, React Compiler, Spring Boot, and PostgreSQL",
-        tech: ["TypeScript", "Java", "React", "React Compiler", "Vite", "Node.js", "Spring Boot", "PostgreSQL", "CSS"],
+        tech: ["TypeScript", "React", "Vite", "Node.js", "Java", "Spring Boot", "Socket.IO", "PostgreSQL", "HTML/CSS"],
         link: "https://github.com/mrlangloi/obs-canvas"
     },
     {
         title: "SFU Graduation Planner (xHacks 2026)",
         description: "A graduation planner for SFU Computing Science students built during a 24-hour hackathon, xHacks 2026",
-        tech: ["JavaScript", "React", "Vite", "Node.js", "CSS"],
+        tech: ["JavaScript", "React", "Vite", "HTML/CSS"],
         link: "https://github.com/mrlangloi/obs-canvas"
     },
     {
@@ -56,7 +56,7 @@ const allProjectsList = [
     {
         title: "OBS Canvas Overlay",
         description: "An Open Broadcaster Software (OBS) overlay for live-stream moderators to display media to viewers",
-        tech: ["JavaScript", "React", "Node.js", "Express", "MongoDB", "CSS"],
+        tech: ["JavaScript", "React", "Node.js", "Express", "MongoDB", "HTML/CSS"],
         link: "https://github.com/mrlangloi/canvas-overlay-client"
     },
     {
